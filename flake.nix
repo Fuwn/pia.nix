28b783842fbc736369be4b809f1725d572b60adc
Adam Washington <adam.washington@stfc.ac.uk>
2021-09-10 17:11:41 +0100

    Full functioning vpn module

--- flake.nix
+++ flake.nix
@@ -1,26 +1,28 @@
 {
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05"; };
   outputs = { self, nixpkgs }: {
-    nixosModules.vpn = { config }: {
+    nixosModule = { lib, config, ... }: {
       options = {
-        services.pia.authUserPass = {
+        services.pia = {
           enable = nixpkgs.lib.mkOption {
             default = false;
             type = nixpkgs.lib.types.bool;
           };
-          username = nixpkgs.lib.mkOption {
-            default = false;
-            type = nixpkgs.lib.types.string;
-          };
-          password = nixpkgs.lib.mkOption {
-            default = false;
-            type = nixpkgs.lib.types.string;
+          authUserPass = {
+            username = nixpkgs.lib.mkOption {
+              default = false;
+              type = nixpkgs.lib.types.str;
+            };
+            password = nixpkgs.lib.mkOption {
+              default = false;
+              type = nixpkgs.lib.types.str;
+            };
           };
         };
       };
       config = nixpkgs.lib.mkIf config.services.pia.enable {
         services.openvpn.servers = let
-          resources = nixpkgs.fetchzip {
+          resources = nixpkgs.legacyPackages.x86_64-linux.fetchzip {
             name = "pia-vpn-config";
             url = "https://www.privateinternetaccess.com/openvpn/openvpn.zip";
             sha256 = "ZA8RS6eIjMVQfBt+9hYyhaq8LByy5oJaO9Ed+x8KtW8=";
