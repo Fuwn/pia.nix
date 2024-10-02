fc6843eb64741ae1763c217f6cf7ede37015bf1c
Fuwn <contact@fuwn.me>
2024-10-02 05:55:22 -0700

    format(flake.nix): reformat

--- flake.nix
+++ flake.nix
@@ -1,48 +1,72 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05"; };
-  outputs = { self, nixpkgs }: {
-    nixosModule = { lib, config, ... }: {
-      options = {
-        services.pia = {
-          enable = nixpkgs.lib.mkOption {
-            default = false;
-            type = nixpkgs.lib.types.bool;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+  outputs =
+    { nixpkgs, self }:
+    {
+      nixosModules.default =
+        { config, ... }:
+        {
+          options.services.pia = {
+            enable = nixpkgs.lib.mkOption {
+              default = false;
+              type = nixpkgs.lib.types.bool;
+            };
+
+            authUserPass = {
+              username = nixpkgs.lib.mkOption {
+                default = false;
+                type = nixpkgs.lib.types.str;
+              };
+
+              password = nixpkgs.lib.mkOption {
+                default = false;
+                type = nixpkgs.lib.types.str;
+              };
+            };
           };
-          authUserPass = {
-            username = nixpkgs.lib.mkOption {
-              default = false;
-              type = nixpkgs.lib.types.str;
-            };
-            password = nixpkgs.lib.mkOption {
-              default = false;
-              type = nixpkgs.lib.types.str;
-            };
+
+          config = nixpkgs.lib.mkIf config.services.pia.enable {
+            services.openvpn.servers =
+              let
+                resources = nixpkgs.legacyPackages.x86_64-linux.fetchzip {
+                  name = "pia-vpn-config";
+                  url = "https://www.privateinternetaccess.com/openvpn/openvpn.zip";
+                  sha256 = "ZA8RS6eIjMVQfBt+9hYyhaq8LByy5oJaO9Ed+x8KtW8=";
+                  stripRoot = false;
+                };
+              in
+              builtins.listToAttrs (
+                map
+                  (name: {
+                    name =
+                      (builtins.replaceStrings
+                        [
+                          ".ovpn"
+                          "_"
+                        ]
+                        [
+                          ""
+                          "-"
+                        ]
+                      )
+                        name;
+
+                    value = {
+                      inherit (config.services.pia) authUserPass;
+
+                      autoStart = false;
+                      config = "config ${resources}/${name}";
+                      updateResolvConf = true;
+                    };
+                  })
+                  (
+                    builtins.filter (name: !((builtins.match ".+ovpn$" name) == null)) (
+                      builtins.attrNames (builtins.readDir resources)
+                    )
+                  )
+              );
           };
         };
-      };
-      config = nixpkgs.lib.mkIf config.services.pia.enable {
-        services.openvpn.servers = let
-          resources = nixpkgs.legacyPackages.x86_64-linux.fetchzip {
-            name = "pia-vpn-config";
-            url = "https://www.privateinternetaccess.com/openvpn/openvpn.zip";
-            sha256 = "ZA8RS6eIjMVQfBt+9hYyhaq8LByy5oJaO9Ed+x8KtW8=";
-            stripRoot = false;
-          };
-          fixup = (builtins.replaceStrings [ ".ovpn" "_" ] [ "" "-" ]);
-          servers =
-            (builtins.filter (name: !(isNull (builtins.match ".+ovpn$" name)))
-              (builtins.attrNames (builtins.readDir resources)));
-          make_server = (name: {
-            name = fixup name;
-            value = {
-              autoStart = false;
-              authUserPass = config.services.pia.authUserPass;
-              config = "config ${resources}/${name}";
-              updateResolvConf = true;
-            };
-          });
-        in builtins.listToAttrs (map make_server servers);
-      };
     };
-  };
 }
